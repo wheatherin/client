bab50e5477181a2c0d93d0d57b95a23c8d98a78e
haekal <haekal.yudh@gmail.com>
2020-01-10 13:14:33 +0700

    feed cuaca

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,49 @@ function onSignIn(googleUser) {
     console.log(data)
     // localStorage.setItem('access_token', data.access_token)
     // $() tembak yang mau di hide
+    $.ajax({
+      url: "http://localhost:3000/api/current/jakarta",//straigh from github
+      method: "get"
+  })
+  .done( data =>{
+      
+      console.log(data,'==============')
+  
+      let today = data.currentResults
+      let url = ""
+      if(today.icon == "rain"){
+        url = "https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather07-512.png"
+      }
+      $('.today').append(`
+      <h1>${today.city}</h1>
+                <img src="${url}" id="mainW" alt="">
+                <h2>${today.weather}</h2>
+                <h5>${today.description}</h5>
+  
+      `)
+  
+      let week = data.weeklyResults
+  
+      $.each(week, function(index, val){
+        $('#feed').append(`
+        <div class="card card-h" style="width: 18rem; border-radius: 10px;">
+        <div class="card-body">
+          <img src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather07-512.png" alt="">
+          <h5 class="card-title">${val.date}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">${val.weather}</h6>
+          <a href="#" class="card-link">UV index ${val.uvIndex}</a>
+          <a href="#" class="card-link">Humidity ${val.humidity}</a>
+        </div>
+      </div>
+        `)
+  
+      })
+  
+  })
+  .fail (err =>{
+      console.log(err)
+  })
+
   })
   .fail(err => {
     console.log(err);
@@ -29,24 +72,6 @@ function signOut() {
 
 $(document).ready(function() {
 
-  $.ajax({
-    url: "http://localhost:3000/api/current/jakarta",//straigh from github
-    method: "get"
-})
-.done( data =>{
-    
-    console.log(data,'==============')
-    // $('#currentUser').append(`
-    // <a class="navbar-brand" href="#">
-    //      ${data.login}
-    //     <img src="${data.avatar_url}" width="30" height="30" class="d-inline-block align-top" alt="">
-    // </a>
 
-    // `)
-    // <p>${data.login}  <img src="${data.avatar_url}" style="width: 2rem; height: 2rem;" alt=""></p>
-})
-.fail (err =>{
-    console.log(err)
-})
 
 })
